1b6287951f2a13f2458463543efe7da73fa677c7
dduvall <dduvall@laptop-t460s.valltel.local>
2023-10-12 23:12:17 -0700

    initial commit of my lazy config from laptop-t460s

---
+++ lua/plugins/tree-sitter.lua
@@ -0,0 +1,43 @@
+-- add more treesitter parsers
+-- add more treesitter parsers
+return
+-- add more treesitter parsers
+{
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "php",
+      "phpdoc",
+      "html",
+      "javascript",
+      "css",
+      "scss",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+      "c_sharp",
+      "odin",
+      "go",
+      "c",
+      "cpp",
+      "pascal",
+      "nasm",
+      "glsl",
+      "gitignore",
+      "erlang",
+      "dart",
+      "java",
+      "kotlin",
+      "arduino",
+    },
+  },
+}
